c297e0986378e7ad03845a1cae756944c77b805b
yohanmanohara <warushayohan80@gmail.com>
2025-03-07 20:39:02 +0530

    make login and sign up

--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -2,6 +2,6 @@ namespace MetroAir.Models;
 
 public class LoginViewModel
 {
-    public required string Username { get; set; }
+    public required string Email { get; set; }
     public required string Password { get; set; }
 }
