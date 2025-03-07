bb4222883906578b58ca2ac97012e01bdadc1a21
Yohan Manohara <warushayohan80@gmail.com>
2025-03-07 21:50:53 +0530

    4 authentication b and  f (#5)
    * make login and sign up
    
    * #
    
    * #
    
    * #
    
    * make login and sign up

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -41,9 +41,9 @@ namespace MetroAir.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(string username, string password)
+        public IActionResult Login(string email, string password)
         {
-            var user = _context.Users.FirstOrDefault(u => u.Username == username);
+            var user = _context.Users.FirstOrDefault(u => u.Email == email);
             if (user != null && PasswordHasher.VerifyPassword(password, user.PasswordHash))
             {
                 // Authentication successful
