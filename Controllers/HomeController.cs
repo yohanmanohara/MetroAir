b0170cdf694d772a9961c9fc09a3f5285d2c9d4d
yohan manohara <kgwyohan80@outlook.com>
2025-03-20 17:04:35 +0530

    made role base file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,23 +19,28 @@ namespace UserRoles.Controllers
             return View();
         }
 
-        [Authorize]
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+       
         [Authorize(Roles = "WebMaster")]
-        public IActionResult Admin()
+        public IActionResult WebMaster()
+        {
+            return View();
+        }
+        [Authorize(Roles = "DataProvider")]
+        public IActionResult DataProvider()
+        {
+            return View();
+        }
+        [Authorize(Roles = "MonitoringAdmin")]
+        public IActionResult MonitoringAdmin()
         {
             return View();
         }
 
-        [Authorize(Roles = "User")]
-        public IActionResult User()
-        {
-            return View();
-        }
+        //[Authorize(Roles = "User")]
+        //public IActionResult User()
+        //{
+        //    return View();
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
